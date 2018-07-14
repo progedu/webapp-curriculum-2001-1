73a716afb2459c18df4a3e47cedd5f2f8d0c8b8e
yukikoya <36853872+yukikoya@users.noreply.github.com>
2018-07-14 17:23:53 +0900

    末尾再帰最適化実施

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,3 +1,23 @@
+import scala.annotation.tailrec
+
 object Main {
 
+  @tailrec
+  def series(n:Int,acc:Int):Int ={
+    if(n == 0){
+      acc
+    }else {
+      series(n - 1,acc + n)
+    }
+  }
+
+  @tailrec
+  def fact(n:Int,acc:Int): Int = {
+    if(n <= 1){
+      acc
+    }else{
+      fact(n - 1,acc * n)
+    }
+  }
 }
+
