c1c3a14be49e16a1632b95b26ab24a9e048ac18c
208-209 <208_209@outlook.jp>
2019-02-26 18:55:38 +0900

    末尾再帰最適化による階乗を求める関数の実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,14 @@
+import scala.annotation.tailrec
+
 object Main {
+
+  def fact(n: Int): Int = {
+    @tailrec
+    def go(n: Int, acc: Int): Int = n match {
+      case 0 => acc
+      case _ => go(n - 1, acc * n)
+    }
+    go(n, 1)
+  }
 
 }
