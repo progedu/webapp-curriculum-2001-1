6e546b8ece181165b23bb8d2873331244c978b5c
syo <M.youta.kwg@gmail.com>
2019-05-07 23:13:47 +0900

    factを末尾再帰最適化されるよう変更

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,12 @@
+import scala.annotation.tailrec
 object Main {
-
+  
+  def fact(n: Int): Int = {
+    @tailrec
+    def loop(n: Int, acc: Int): Int = n match {
+      case _ if n <= 1 => acc
+      case _ => loop(n - 1, n * acc)
+    }
+    loop(n, 1)
+  }
 }
