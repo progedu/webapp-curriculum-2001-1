60ed32241d974f9345c363863b9f8fa9e9ad6207
QWYNG <ikusawasi@gmail.com>
2020-03-14 15:11:01 +0900

    impl factorial

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,11 @@
+import scala.annotation.tailrec
+
 object Main {
 
+    def factorial(n: Int): Int = {
+        fact(n, 1)
+    }
+
+    @tailrec
+    def fact(n: Int, acc: Int): Int = if (n <= 1) acc else fact(n - 1, acc * n)
 }
